ae0a198912172bcf053a8a868a7a021c80e8046d
Dakota Solis <dakota.h.solis@gmail.com>
2021-06-30 20:59:59 -0500

    Got a form setup with placeholder cards before I begin plugging stuff in. Realized I needed to have the form setup to begin figuring out how to plug in data and create cards with objects and array.

---
+++ lib.js
@@ -0,0 +1,11 @@
+const newBookCard = document.createElement('div');
+const cardText = document.createElement('p');
+const container = document.getElementById('container')
+for (i=0; i<2; i++){
+    newBookCard;
+    cardText;
+    newBookCard.innerHTML = 'womp';
+    cardText.innerHTML = 'wompwomp';
+    document.container.appendChild(newBookCard)
+    document.container.appendChild(cardText);
+}
