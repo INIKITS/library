501f9b886fa598f14b9188f9d2ba46f915a55ef5
Dakota Solis <dakota.h.solis@gmail.com>
2021-07-01 20:02:54 -0500

    Worked out how to get form data into an array, but wasn't how TOP specified, so trying to figure out how to get form data in their way also.

--- lib.js
+++ lib.js
@@ -1,11 +1,53 @@
-const newBookCard = document.createElement('div');
-const cardText = document.createElement('p');
-const container = document.getElementById('container')
-for (i=0; i<2; i++){
-    newBookCard;
-    cardText;
-    newBookCard.innerHTML = 'womp';
-    cardText.innerHTML = 'wompwomp';
-    document.container.appendChild(newBookCard)
-    document.container.appendChild(cardText);
+// const newBookCard = document.createElement('div');
+// const cardText = document.createElement('p');
+// const container = document.getElementById('container')
+// for (i=0; i<2; i++){
+//     newBookCard;
+//     cardText;
+//     newBookCard.innerHTML = 'womp';
+//     cardText.innerHTML = 'wompwomp';
+//     document.container.appendChild(newBookCard)
+//     document.container.appendChild(cardText);
+// }
+const bookForm = document.getElementById('bookinfo');
+const bookDetails = bookForm.elements;
+const myLibrary = []
+var submitButton = document.getElementById('submit');
+
+function book(){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = false;
 }
+
+book.prototype.info = function(){
+    return(title + ' by ' + author + ', ' + pages + ' pages. ' + read)
+}
+
+
+function addBookToLibrary(){
+        // e.preventDefault();
+        var data = new FormData(bookForm);
+        for (const [name,value] of data) {
+            console.log(name,value)
+            myLibrary.push(name,value);
+        }
+}
+
+bookForm.addEventListener('submit', addBookToLibrary(bookForm))
+console.log(myLibrary);
+
+
+
+
+
+
+
+// const bookDetails = bookForm.elements;
+// bookDetails.forEach(function (info, index){
+//     console.log(index);
+//     console.log(info);
+// })
+// console.log(bookDetails);
+
